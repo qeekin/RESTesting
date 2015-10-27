33058e4e2c4dd8c30060473bc704f977741749f8
Chiahao Lin <y80811@gmail.com>
2015-10-27 19:14:12 +0800

    Add "tester gen" command to create a json from url

--- boot.js
+++ boot.js
@@ -5,9 +5,23 @@ var spawn = require('child_process').spawn;
 var request = require('request');
 var path = require('path');
 var url = require('url');
+var fs = require('fs');
 
-function gen_json(objs) {
-
+function gen_json(_url, fpath, callback) {
+  request.get(_url, function(err, res, body) {
+    if(err) return console.log('Bad request');
+    try {
+      var obj = JSON.parse(body);
+      fpath = path.resolve(fpath);
+      fs.writeFile(fpath, JSON.stringify(obj), function(err) {
+        console.log('A json file has been created.');
+        process.exit();
+      })
+    } catch(err) {
+      console.log('The result cannot parse to json.');
+      process.exit();
+    }
+  });
 }
 
 function run_mocha(type, fpath) {
@@ -26,7 +40,22 @@ program
   .option('-f, --file <File>', 'Specify an alternate json file')
   .option('-m, --multi <File>', 'Specify an alternate json file with multi scenarios')
   .option('-d, --directory <Dir>', 'Specify an alternate directory')
-  .parse(process.argv);
+
+program
+  .command('gen <url>')
+  .description('Create a json file from the url')
+  .option('-o, --output <Path>', 'Export path')
+  .action(function(_url, options) {
+    var fpath = options.output;
+    if(fpath) {
+      gen_json(_url, fpath, function() {});
+    } else {
+      console.log('Need to specify a output file');
+      process.exit();
+    }
+  });
+
+program.parse(process.argv);
 
 if (program.file) {
   run_mocha('file', program.file);
